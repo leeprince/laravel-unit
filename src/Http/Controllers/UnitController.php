020de56e55eb00664e6b8b51282e18b1e7f0fd54
leeprince <prince.lee@fowo.com>
2020-12-08 17:36:14 +0800

    feature: 添加环境检测 | 仅允许本地开发环境使用

--- src/Http/Controllers/UnitController.php
+++ src/Http/Controllers/UnitController.php
@@ -12,6 +12,18 @@ use Illuminate\Http\Request;
 
 class UnitController extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        
+        // 安全防护：环境检测，禁止生产环境使用
+        if (config('app.env') != 'local'
+            || !config('app.debug')
+        ) {
+            dd('prince: 403 forbidden');
+        }
+    }
+    
     /**
      * [测试窗口]
      *
