5c63d84b9ba25b6b6542da4ac4f91dbfce62145f
leeprince <leeprince@foxmail.com>
2021-02-01 00:36:45 +0800

    feature:优化类型、请求方式 ｜ 执行类型强制转换、ajax 请求

--- src/Http/Controllers/UnitController.php
+++ src/Http/Controllers/UnitController.php
@@ -30,7 +30,9 @@ class UnitController extends Controller
     public function index()
     {
         // dump('Unit::index');
-        return view("unitview::index");
+        
+        $assetPath = '/vendor/leeprince/laravel-unit';
+        return view("unitview::index", ['assetPath' => $assetPath]);
     }
     
     /**
@@ -62,18 +64,73 @@ class UnitController extends Controller
         ], [
             'namespace' => '「命名空间」'
         ]);*/
+    
+        $object = $this->getFullClass($namespace, $className);
+    
+        $paramArr = $this->getParamArr($param);
+    
+        try {
+            $data  = call_user_func_array([$object, $action], $paramArr);
+            return $data;
+        } catch (\Exception $e) {
+            dd('捕获异常：', $e);
+        }
+    }
+    
+    /**
+     * [获取请求的参数数组]
+     * @param string $param
+     * @return array
+     */
+    public function getParamArr(string $param): array
+    {
+        $paramArr = [];
+        if (empty($param)) {
+            return $paramArr;
+        }
+        $paramArr = explode('|', $param);
+        foreach ($paramArr as &$item) {
+            $item = $this->getTypeOfValue($item);
+        }
+        return $paramArr;
+    }
+    
+    /**
+     * [获取的类型转换后的值]
+     *  强制转换同php的基本写法一致:(string/int/array)值，特别注意的是强制转为array的值必须是json字符串
+     *  php 特性：数字转换为字符串类型或者整型类型 传入 字符串或者整型的类型检查是不会报错的
+     */
+    public function getTypeOfValue(string $value)
+    {
+        if (! preg_match("/\((.*)\)(.*)/", $value, $match)) {
+            return $value;
+        }
+        $type = $match[1]; // 强制转换类型
+        $noConvertValue = $match[2]; // 原始字符串数据
+        if (! array_key_exists($type, self::$valueType)) {
+            return $value;
+        }
+        $convFunc = self::$valueType[$type]; // 方法
+        if ($type == self::VALUE_ARRAY) { // 强制转换为数组特殊处理
+            return $convFunc($noConvertValue, true);
+        }
         
+        $value = $convFunc($noConvertValue);
+        return $value;
+    }
+    
+    /**
+     * [获取被测试的完整类实例]
+     * @param string $namespace
+     * @param string $className
+     * @return string
+     */
+    public function getFullClass(string $namespace, string $className)
+    {
         $class  = empty($className) ? $namespace : $namespace . '\\' . $className;
         $class  = str_replace("/", '\\', $class);
-        $object = new $class();
-    
-        $paramArr = [];
-        if (!empty($param)) {
-            $paramArr = explode('|', $param);
-            $paramArr = array_map('trim', $paramArr);
-        }
-        $data  = call_user_func_array([$object, $action], $paramArr);
+        $class  = str_replace(";", '', $class);
         
-        return (is_array($data)) ? json_encode($data) : dd($data);
+        return new $class();
     }
 }
